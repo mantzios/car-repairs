3844a8918a4b1b0a271441fb01133e26f6726268
Angeliki <kalyva.aggeliki@gmail.com>
2017-10-07 11:46:05 +0300

    first commit

---
+++ src/main/java/com/teamnine/carrepairs/CarRepairsApplication.java
@@ -0,0 +1,12 @@
+package com.teamnine.carrepairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRepairsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRepairsApplication.class, args);
+	}
+}

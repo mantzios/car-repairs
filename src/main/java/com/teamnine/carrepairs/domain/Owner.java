0c9f2fdf0b50a805fee7d30b48884aac9e97be68
Angeliki <kalyva.aggeliki@gmail.com>
2017-10-12 20:05:05 +0300

    Second commit, controller and ftl pages, just routing!

---
+++ src/main/java/com/teamnine/carrepairs/domain/Owner.java
@@ -0,0 +1,27 @@
+package com.teamnine.carrepairs.domain;
+
+//import javax.persistence.*;
+//
+//import java.io.Serializable;
+//
+//public class Owner implements Serializable {
+//
+//    @Id
+//    @Column(name = "Owner_id", nullable =
+//            false)
+//    @GeneratedValue(strategy =
+//            GenerationType.AUTO)
+//    private long ownerid;
+//    @Column(name = "afm_number", nullable =
+//            false)
+//    private int afm;
+//    @Column(name = "name", nullable =
+//            false)
+//    private int name;
+//    @Column(name = "lastname", nullable =
+//            false)
+//    private int lastname;
+//
+//
+//
+//}

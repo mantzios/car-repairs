93072a597440142587f671490408cf19f6229ac7
mantzios <apo.mantzios@gmail.com>
2017-10-14 12:04:05 +0300

    Push

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 INSERT into owners(afm_number,email,password,access) VALUES ("748578235","apo.mantzios@gmail.com","12345","ADMIN");
 INSERT into owners(access, afm_number, email, lastname, name, password) VALUES ("USER",19298491,"aggeliki@gmail.com","agg","agg","1244");
+INSERT into repair(cost, datetime, status, textarea, type, owner_id, vehicle_id) VALUES (200,"2017-6-6","sta","","a","1","3");

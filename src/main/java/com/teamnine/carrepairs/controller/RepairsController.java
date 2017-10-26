4ec8e20f18026743f22f9e705bb59c7d6b5a1872
mantzios <apo.mantzios@gmail.com>
2017-10-27 00:03:22 +0300

    er

--- src/main/java/com/teamnine/carrepairs/controller/RepairsController.java
+++ src/main/java/com/teamnine/carrepairs/controller/RepairsController.java
@@ -12,6 +12,7 @@ import com.teamnine.carrepairs.service.AccountService;
 import com.teamnine.carrepairs.service.RepairService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
